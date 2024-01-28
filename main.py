e4d824cca01d592615bade33c0768f7f72e68afa
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2024-01-28 01:04:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ from tensorflow.image import resize
 from tensorflow.keras.models import load_model, save_model
 
 
-with st.sidebar:
-    st.title("Home Page")
-    selection=st.radio("select your option",options=["upload an image", "Insert Image url"])
+
+st.title("Home Page")
+
 
 def sort_extension():
     
@@ -87,82 +87,6 @@ def saving_into_dataFrame():
 # import numpy as np
 # from io import BytesIO
 
-def download_and_save_image(image_url, save_path="downloaded_image.png"):
-    '''response = requests.get(image_url)
-    if response.status_code == 200:
-        image_data = BytesIO(response.content)
-        img = Image.open(image_data).convert("RGB")
-        saved_img_path="./"+save_path
-        img.save(saved_img_path)
-        resize_img = resize(img, (150, 150))
-    
-        img_array = img_to_array(resize_img)
-        img_array = np.expand_dims(img_array, axis=0)
-    
-        img_array_copy = img_array.copy()
-        img_array_copy /= 255.0
-    
-        loaded_model = load_model("Waste_Management_Model.h5")
-    
-        if st.button("Predict"):
-            prediction = loaded_model.predict(img_array_copy)
-            predicted_class = np.argmax(prediction)
-            
-            class_labels = {0: 'cardboard', 1: 'metal', 2: 'paper', 3: 'plastic'}
-            predicted_category = class_labels[predicted_class]
-            
-            result = f"This Item is a {predicted_category}"
-            st.success(result)
-            st.image(img, caption=None)
-    
-        
-    else:
-        st.error(f"Failed to download the image. Status code: {response.status_code}")'''
-
-    try:
-        response = requests.get(image_url)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
-    
-        image_data = BytesIO(response.content)
-        img = Image.open(image_data).convert("RGB")
-        saved_img_path = "./" + save_path
-        img.save(saved_img_path)
-        resize_img = resize(img, (1822,1275))
-    
-        img_array = img_to_array(resize_img)
-        img_array = np.expand_dims(img_array, axis=0)
-    
-        img_array_copy = img_array.copy()
-        img_array_copy /= 255.0
-
-        loaded_model = load_model("Waste_Management_Model.h5")
-
-        if st.button("Predict"):
-            prediction = loaded_model.predict(img_array_copy)
-            predicted_class = np.argmax(prediction)
-    
-            class_labels = {0: 'Normal', 1: 'Bacteria', 2: 'Virus'}
-            predicted_category = class_labels[predicted_class]
-    
-            result = f"This Xray is a {predicted_category}"
-            st.success(result)
-            st.image(img, caption=None)
-
-    except requests.exceptions.HTTPError as errh:
-        st.error(f"HTTP Error: {errh}")
-    except requests.exceptions.ConnectionError as errc:
-        st.error(f"Error Connecting: {errc}")
-    except requests.exceptions.RequestException as err:
-        st.error(f"Failed to download the image, The Image link is not a downloadble link")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-
-def image_url_input():
-    st.title("Classification with URL")
-    image_url = st.text_input("Enter the image url and press enter", key="akaska")    
-    # if st.button("Download Image"):
-    if image_url != "":
-        download_and_save_image(image_url)
 
 
 
@@ -184,7 +108,7 @@ def insert():
         # Convert the uploaded image to RGB
         img = Image.open(uploaded_file).convert("RGB")
         # Resize the image using TensorFlow
-        resize_img = resize(img, (150, 150))
+        resize_img = resize(img, (1822,1275))
 
         # Convert the resized image to an array
         img_array = img_to_array(resize_img)
@@ -196,7 +120,7 @@ def insert():
 
 
         # To load the model
-        loaded_model = load_model("Waste_Management_Model.h5")
+        loaded_model = load_model("pneumonia_best_model.h5")
         # Make the prediction
         
         if st.button("Predict"):
